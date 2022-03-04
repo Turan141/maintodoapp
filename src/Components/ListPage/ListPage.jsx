94d412dbfdc445ff95793ca77fc3fa39ba85d194
Your Name <you@example.com>
2022-03-04 17:51:32 +0400

    added delete button and true/false switch

--- src/Components/ListPage/ListPage.jsx
+++ src/Components/ListPage/ListPage.jsx
@@ -3,19 +3,20 @@ import getUserTodo from "../Service";
 import ToDoTable from "./UserListTable";
 
 const ListPage = () => {
-  const [fetchedData, setFetchedData] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [todos, setTodos] = useState([...localStorage.getItem("todos")]);
 
   useEffect(() => {
     (function toDoList() {
       getUserTodo().then((data) => {
-        setFetchedData(data.data);
         setLoading(false);
+        setTodos(data.data);
+        localStorage.setItem("todos", JSON.stringify(data.data));
       });
     })();
   }, []);
 
-  return <>{!isLoading ? <ToDoTable fetchedData={fetchedData} /> : null}</>;
+  return <>{!isLoading ? <ToDoTable fetchedData={todos} /> : null}</>;
 };
 
 export default ListPage;
